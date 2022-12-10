a45f82f7514649a30469a728868c6ec8d84aa80d
Stefan Kerkewitz <stefan.kerkewitz@gmail.com>
2022-12-10 13:43:56 +0100

    Day 10 (cleanup)

--- Day10/main.py
+++ Day10/main.py
@@ -5,7 +5,7 @@ lines = open("real.txt", "r").read().split('\n')
 cycles = 0
 register = 1
 
-result = []
+input_data = []
 for l in lines:
     if l == 'noop':
         cycles += 1
@@ -14,8 +14,7 @@ for l in lines:
         register += p
         cycles += 2
 
-    print(f'Cycles {cycles} value {register}')
-    result.append((cycles, register))
+    input_data.append((cycles, register))
 
 
 def register_at_cycle(l, c):
@@ -30,31 +29,28 @@ def signal_strength(l, c):
     return register_at_cycle(l, c) * c
 
 
-print()
-
+# Part 1
 check_points = [20, 60, 100, 140, 180, 220]
 result1: int = 0
 for i in check_points:
-    strength: int = signal_strength(result, i)
-    print(f'Signal at {i} is {strength}')
+    strength: int = signal_strength(input_data, i)
     result1 += strength
 
 
 print(f'Result part 1: {result1}')
-# print(f'Result part 1: {simulate(10, lines)}')
 
-
+# Part 2
 crt: str = ''
 sprite_pos = 1
 for i in range(0, 240):
-    sprite_pos = register_at_cycle(result, i + 1)
+    sprite_pos = register_at_cycle(input_data, i + 1)
     crt_pos = (i % 40) + 1
     if sprite_pos <= crt_pos < sprite_pos + 3:
         crt += '#'
     else:
         crt += ' '
 
-
 crt_lines = [crt[idx:idx + 40] for idx in range(0, len(crt), 40)]
+print(f'Result part 2:')
 for l in crt_lines:
     print(l)
